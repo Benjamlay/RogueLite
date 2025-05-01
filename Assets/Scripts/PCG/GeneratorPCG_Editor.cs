83c7bf877aed3e2d0dcc26def1b925fc6ee24c01
benjamin layson <benjaminlayson0@gmail.com>
2025-05-01 13:43:47 +0200

    le jeu build

--- Assets/Scripts/PCG/GeneratorPCG_Editor.cs
+++ Assets/Scripts/PCG/GeneratorPCG_Editor.cs
@@ -1,30 +1,30 @@
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(GeneratorPCG))]
+//[CustomEditor(typeof(GeneratorPCG))]
 
-public class GeneratorPCG_Editor : Editor
-{
+// public class GeneratorPCG_Editor : Editor
+// {
 
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-        
-        GeneratorPCG generatorPcg = (GeneratorPCG)target;
-        
-        EditorGUILayout.Space(20);
-        
-        if (GUILayout.Button("Generate"))
-        {
-            //Debug.Log("Generate baby !!!");
-            generatorPcg.generationCoroutine = true;
-            generatorPcg.StartGenerate();
-        }
-
-        if (GUILayout.Button("Clear all"))
-        {
-            generatorPcg.StopAllCoroutines();
-            generatorPcg.ClearMap();
-        }
-    }
-}
+    // public override void OnInspectorGUI()
+    // {
+    //     DrawDefaultInspector();
+    //     
+    //     GeneratorPCG generatorPcg = (GeneratorPCG)target;
+    //     
+    //     EditorGUILayout.Space(20);
+    //     
+    //     if (GUILayout.Button("Generate"))
+    //     {
+    //         //Debug.Log("Generate baby !!!");
+    //         generatorPcg.generationCoroutine = true;
+    //         generatorPcg.StartGenerate();
+    //     }
+    //
+    //     if (GUILayout.Button("Clear all"))
+    //     {
+    //         generatorPcg.StopAllCoroutines();
+    //         generatorPcg.ClearMap();
+    //     }
+    // }
+//}
